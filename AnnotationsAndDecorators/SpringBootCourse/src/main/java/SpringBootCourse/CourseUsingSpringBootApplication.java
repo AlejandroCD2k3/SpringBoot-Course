7ab186aa0490cff2ef4be27df66d557161e2e193
AlejandroCD2k3 <alejandrocd2k3@gmail.com>
2024-11-26 22:59:37 -0500

    feat: Adding Annotations and Decorators folder

---
+++ AnnotationsAndDecorators/SpringBootCourse/src/main/java/SpringBootCourse/CourseUsingSpringBootApplication.java
@@ -0,0 +1,13 @@
+package SpringBootCourse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseUsingSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseUsingSpringBootApplication.class, args);
+	}
+
+}
