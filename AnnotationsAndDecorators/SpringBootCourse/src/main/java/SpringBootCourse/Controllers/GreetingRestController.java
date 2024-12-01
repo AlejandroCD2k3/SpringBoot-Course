2c7327769178433cb10dd6578eca2ffd683ac5a5
AlejandroCD2k3 <alejandrocd2k3@gmail.com>
2024-11-30 22:38:28 -0500

    feat: Adding PathVariable decorators simple example

---
+++ AnnotationsAndDecorators/SpringBootCourse/src/main/java/SpringBootCourse/Controllers/GreetingRestController.java
@@ -0,0 +1,15 @@
+package SpringBootCourse.Controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GreetingRestController {
+
+    @GetMapping("/greeting/{name}")
+    public String greeting(@PathVariable String name){
+        return ("Hi!, " + name);
+    }
+
+}
